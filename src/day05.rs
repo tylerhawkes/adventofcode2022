e8dfc027ed740974f70a7c1e26ee93a9b43a8908
Tyler Hawkes <tyler.hawkes@gmail.com>
2022-12-04 22:37:54 -0700

    Day05 complete

---
+++ src/day05.rs
@@ -0,0 +1,77 @@
+use std::str::FromStr;
+
+pub fn compute(s: &str) -> (usize, usize) {
+  let mut lines = s.lines();
+  let mut stacks = parse_stacks(&mut lines);
+  let mut stacks2 = stacks.clone();
+  let moves = parse_moves(&mut lines);
+  let mut transient = vec![];
+  for mv in moves {
+    transient.clear();
+    for _ in 0..mv.count {
+      let container = stacks[mv.from].pop().unwrap();
+      stacks[mv.to].push(container);
+      let container = stacks2[mv.from].pop().unwrap();
+      transient.push(container);
+    }
+    transient
+      .iter()
+      .rev()
+      .copied()
+      .for_each(|c| stacks2[mv.to].push(c));
+  }
+  let arrangement = stacks
+    .iter()
+    .map(|s| s.last().copied().unwrap_or(' '))
+    .collect::<String>();
+  println!("{arrangement}");
+  let arrangement2 = stacks2
+    .iter()
+    .map(|s| s.last().copied().unwrap_or(' '))
+    .collect::<String>();
+  println!("{arrangement2}");
+  // Not FWDFHVZWJ
+  (0, 0)
+}
+
+fn parse_stacks<'a, I: Iterator<Item = &'a str>>(i: &mut I) -> Vec<Vec<char>> {
+  let mut stacks = vec![vec![]; 9];
+  for line in i {
+    if line.is_empty() {
+      stacks.iter_mut().for_each(|v| v.reverse());
+      return stacks;
+    }
+    for (i, c) in line.chars().enumerate() {
+      if c.is_ascii_uppercase() {
+        stacks[(i - 1) / 4].push(c);
+      }
+    }
+  }
+  unreachable!()
+}
+
+fn parse_moves<'a, I: Iterator<Item = &'a str>>(i: &mut I) -> Vec<Move> {
+  let mut moves = vec![];
+  for line in i {
+    moves.push(line.parse().unwrap());
+  }
+  moves
+}
+
+#[derive(Debug)]
+struct Move {
+  count: usize,
+  from: usize,
+  to: usize,
+}
+
+impl FromStr for Move {
+  type Err = std::convert::Infallible;
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    let split = s.split_whitespace().collect::<Vec<_>>();
+    let count = split[1].parse().unwrap();
+    let from = split[3].parse::<usize>().unwrap() - 1;
+    let to = split[5].parse::<usize>().unwrap() - 1;
+    Ok(Move { count, from, to })
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 // mod day01;
 // mod day02;
 // mod day03;
-mod day04;
+// mod day04;
+mod day05;
 
 fn main() {
   // println!("{:?}", day01::compute(include_str!("../input/day01.txt")));
   // println!("{:?}", day02::compute(include_str!("../input/day02.txt")));
   // println!("{:?}", day03::compute(include_str!("../input/day03.txt")));
-  println!("{:?}", day04::compute(include_str!("../input/day04.txt")));
+  // println!("{:?}", day04::compute(include_str!("../input/day04.txt")));
+  println!("{:?}", day05::compute(include_str!("../input/day05.txt")));
 }
