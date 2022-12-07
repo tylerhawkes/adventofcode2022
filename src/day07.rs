b209cc094b7349cfd9baa05a6be476b013844359
Tyler Hawkes <tyler.hawkes@gmail.com>
2022-12-06 23:05:06 -0700

    Day07 complete

--- src/day07.rs
+++ src/day07.rs
@@ -1,3 +1,126 @@
+use std::{collections::BTreeMap, str::FromStr};
+
 pub fn compute(s: &str) -> (usize, usize) {
-  (0, 0)
+  let dir = s.parse::<Directory>().unwrap();
+  (
+    dir.total_sizes_under_n(100_000),
+    dir.smallest_size_over_n(dir.total_size - 40_000_000),
+  )
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+struct Directory {
+  total_size: usize,
+  nodes: BTreeMap<String, Node>,
+}
+
+impl Directory {
+  fn dirs(&self) -> impl Iterator<Item = &Self> {
+    self
+      .nodes
+      .values()
+      .filter_map(|n| if let Node::Dir(d) = n { Some(d) } else { None })
+  }
+  fn compute_size(&mut self) -> usize {
+    let mut size = 0;
+    for node in self.nodes.values_mut() {
+      match node {
+        Node::File(u) => size += *u,
+        Node::Dir(dir) => size += dir.compute_size(),
+      }
+    }
+    self.total_size = size;
+    size
+  }
+  fn total_sizes_under_n(&self, n: usize) -> usize {
+    let mut size = if self.total_size < n {
+      self.total_size
+    } else {
+      0
+    };
+    for d in self.dirs() {
+      size += d.total_sizes_under_n(n);
+    }
+    size
+  }
+  fn smallest_size_over_n(&self, n: usize) -> usize {
+    let mut smallest = if self.total_size >= n {
+      self.total_size
+    } else {
+      usize::MAX
+    };
+    for d in self.dirs() {
+      if d.total_size >= n {
+        smallest = smallest.min(d.smallest_size_over_n(n));
+      }
+    }
+    smallest
+  }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+enum Node {
+  Dir(Directory),
+  File(usize),
+}
+
+impl Node {
+  fn unwrap_dir(&mut self) -> &mut Directory {
+    match self {
+      Self::Dir(dir) => dir,
+      Self::File(_) => unreachable!(),
+    }
+  }
+}
+
+impl FromStr for Directory {
+  type Err = ::core::convert::Infallible;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    let mut base = Directory {
+      total_size: 0,
+      nodes: BTreeMap::new(),
+    };
+    let mut current_dir = &mut base;
+    let mut dir_stack = vec![];
+    for line in s.lines() {
+      match line {
+        "$ cd /" => {
+          dir_stack.clear();
+          current_dir = &mut base
+        }
+        "$ cd .." => {
+          dir_stack.pop().unwrap();
+          current_dir = &mut base;
+          for dir in &dir_stack {
+            current_dir = current_dir.nodes.get_mut(dir).unwrap().unwrap_dir();
+          }
+        }
+        cd if cd.starts_with("$ cd ") => {
+          let dir_name = cd[5..].to_string();
+          current_dir = current_dir.nodes.get_mut(&dir_name).unwrap().unwrap_dir();
+          dir_stack.push(dir_name);
+        }
+        "$ ls" => {}
+        dir if dir.starts_with("dir") => {
+          current_dir.nodes.insert(
+            dir[4..].to_string(),
+            Node::Dir(Directory {
+              total_size: 0,
+              nodes: BTreeMap::new(),
+            }),
+          );
+        }
+        file => {
+          let parts = file.split_whitespace().collect::<Vec<_>>();
+          assert_eq!(parts.len(), 2);
+          current_dir
+            .nodes
+            .insert(parts[1].to_string(), Node::File(parts[0].parse().unwrap()));
+        }
+      }
+    }
+    base.compute_size();
+    Ok(base)
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   if args.is_empty() {
     return Err("pass at least one day".into());
   }
-  let days: Vec<usize> = if args[0] == "all" {
+  let days: Vec<usize> = if args.first().map(|s| s.as_str()) == Some("all") {
     (1..=25).collect()
   } else {
     args
@@ -43,31 +43,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   let start = Instant::now();
   for day in days {
     match day {
-      1 => print_result(day01::compute(include_str!("../input/day01.txt"))),
-      2 => print_result(day02::compute(include_str!("../input/day02.txt"))),
-      3 => print_result(day03::compute(include_str!("../input/day03.txt"))),
-      4 => print_result(day04::compute(include_str!("../input/day04.txt"))),
-      5 => print_result(day05::compute(include_str!("../input/day05.txt"))),
-      6 => print_result(day06::compute(include_str!("../input/day06.txt"))),
-      7 => print_result(day07::compute(include_str!("../input/day07.txt"))),
-      8 => print_result(day08::compute(include_str!("../input/day08.txt"))),
-      9 => print_result(day09::compute(include_str!("../input/day09.txt"))),
-      10 => print_result(day10::compute(include_str!("../input/day10.txt"))),
-      11 => print_result(day11::compute(include_str!("../input/day11.txt"))),
-      12 => print_result(day12::compute(include_str!("../input/day12.txt"))),
-      13 => print_result(day13::compute(include_str!("../input/day13.txt"))),
-      14 => print_result(day14::compute(include_str!("../input/day14.txt"))),
-      15 => print_result(day15::compute(include_str!("../input/day15.txt"))),
-      16 => print_result(day16::compute(include_str!("../input/day16.txt"))),
-      17 => print_result(day17::compute(include_str!("../input/day17.txt"))),
-      18 => print_result(day18::compute(include_str!("../input/day18.txt"))),
-      19 => print_result(day19::compute(include_str!("../input/day19.txt"))),
-      20 => print_result(day20::compute(include_str!("../input/day20.txt"))),
-      21 => print_result(day21::compute(include_str!("../input/day21.txt"))),
-      22 => print_result(day22::compute(include_str!("../input/day22.txt"))),
-      23 => print_result(day23::compute(include_str!("../input/day23.txt"))),
-      24 => print_result(day24::compute(include_str!("../input/day24.txt"))),
-      25 => print_result(day25::compute(include_str!("../input/day25.txt"))),
+      0 => {}
+      1 => print_result(day01::compute, include_str!("../input/day01.txt")),
+      2 => print_result(day02::compute, include_str!("../input/day02.txt")),
+      3 => print_result(day03::compute, include_str!("../input/day03.txt")),
+      4 => print_result(day04::compute, include_str!("../input/day04.txt")),
+      5 => print_result(day05::compute, include_str!("../input/day05.txt")),
+      6 => print_result(day06::compute, include_str!("../input/day06.txt")),
+      7 => print_result(day07::compute, include_str!("../input/day07.txt")),
+      8 => print_result(day08::compute, include_str!("../input/day08.txt")),
+      9 => print_result(day09::compute, include_str!("../input/day09.txt")),
+      10 => print_result(day10::compute, include_str!("../input/day10.txt")),
+      11 => print_result(day11::compute, include_str!("../input/day11.txt")),
+      12 => print_result(day12::compute, include_str!("../input/day12.txt")),
+      13 => print_result(day13::compute, include_str!("../input/day13.txt")),
+      14 => print_result(day14::compute, include_str!("../input/day14.txt")),
+      15 => print_result(day15::compute, include_str!("../input/day15.txt")),
+      16 => print_result(day16::compute, include_str!("../input/day16.txt")),
+      17 => print_result(day17::compute, include_str!("../input/day17.txt")),
+      18 => print_result(day18::compute, include_str!("../input/day18.txt")),
+      19 => print_result(day19::compute, include_str!("../input/day19.txt")),
+      20 => print_result(day20::compute, include_str!("../input/day20.txt")),
+      21 => print_result(day21::compute, include_str!("../input/day21.txt")),
+      22 => print_result(day22::compute, include_str!("../input/day22.txt")),
+      23 => print_result(day23::compute, include_str!("../input/day23.txt")),
+      24 => print_result(day24::compute, include_str!("../input/day24.txt")),
+      25 => print_result(day25::compute, include_str!("../input/day25.txt")),
       _ => return Err("Invalid day".into()),
     }
   }
@@ -76,6 +77,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   Ok(())
 }
 
-fn print_result(r: (impl Display, impl Display)) {
+fn print_result<D1: Display, D2: Display, F: Fn(&str) -> (D1, D2)>(f: F, s: &str) {
+  let start = Instant::now();
+  let r = f(s);
+  let end = Instant::now();
   println!("{}, {}", r.0, r.1);
+  println!("took: {:?}", end - start);
 }
