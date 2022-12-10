01bf2737311527d8c775bd213630ffba245fb0ee
Tyler Hawkes <tyler.hawkes@gmail.com>
2022-12-09 22:55:47 -0700

    Day10 complete

--- src/day10.rs
+++ src/day10.rs
@@ -1,3 +1,37 @@
-pub fn compute(s: &str) -> (usize, usize) {
-  (0, 0)
+pub fn compute(s: &str) -> (isize, String) {
+  let mut x = 1_isize;
+  let mut cycle = 1;
+  let mut signal_strengths = 0;
+  let cycles_for_strengths = vec![20, 60, 100, 140, 180, 220];
+  let mut crt = vec![' '; 240];
+  for line in s.lines() {
+    let (cycle_count, adder) = match line {
+      "noop" => (1_isize, 0_isize),
+      _ => (2, line[5..].parse::<isize>().unwrap()),
+    };
+    // println!("before: {cycle_count}, {cycle}, {adder}, {x}");
+
+    // println!("after : {cycle_count}, {cycle}, {adder}, {x}");
+
+    if let Some(cycle_for_strength) = cycles_for_strengths
+      .iter()
+      .find(|s| (cycle..cycle + cycle_count).contains(s))
+    {
+      signal_strengths += x * *cycle_for_strength;
+    }
+    for i in 0..cycle_count {
+      if (x..=x + 2).contains(&((cycle + i) % 40)) {
+        crt[(cycle + i) as usize - 1] = '#';
+      }
+    }
+
+    cycle += cycle_count;
+    x += adder;
+  }
+  let mut s = "".to_string();
+  for line in crt.chunks(40) {
+    s.extend(line);
+    s.push('\n');
+  }
+  (signal_strengths, s)
 }
--- src/main.rs
+++ src/main.rs
@@ -44,31 +44,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   for day in days {
     match day {
       0 => {}
-      1 => print_result(day01::compute, include_str!("../input/day01.txt")),
-      2 => print_result(day02::compute, include_str!("../input/day02.txt")),
-      3 => print_result(day03::compute, include_str!("../input/day03.txt")),
-      4 => print_result(day04::compute, include_str!("../input/day04.txt")),
-      5 => print_result(day05::compute, include_str!("../input/day05.txt")),
-      6 => print_result(day06::compute, include_str!("../input/day06.txt")),
-      7 => print_result(day07::compute, include_str!("../input/day07.txt")),
-      8 => print_result(day08::compute, include_str!("../input/day08.txt")),
-      9 => print_result(day09::compute, include_str!("../input/day09.txt")),
-      10 => print_result(day10::compute, include_str!("../input/day10.txt")),
-      11 => print_result(day11::compute, include_str!("../input/day11.txt")),
-      12 => print_result(day12::compute, include_str!("../input/day12.txt")),
-      13 => print_result(day13::compute, include_str!("../input/day13.txt")),
-      14 => print_result(day14::compute, include_str!("../input/day14.txt")),
-      15 => print_result(day15::compute, include_str!("../input/day15.txt")),
-      16 => print_result(day16::compute, include_str!("../input/day16.txt")),
-      17 => print_result(day17::compute, include_str!("../input/day17.txt")),
-      18 => print_result(day18::compute, include_str!("../input/day18.txt")),
-      19 => print_result(day19::compute, include_str!("../input/day19.txt")),
-      20 => print_result(day20::compute, include_str!("../input/day20.txt")),
-      21 => print_result(day21::compute, include_str!("../input/day21.txt")),
-      22 => print_result(day22::compute, include_str!("../input/day22.txt")),
-      23 => print_result(day23::compute, include_str!("../input/day23.txt")),
-      24 => print_result(day24::compute, include_str!("../input/day24.txt")),
-      25 => print_result(day25::compute, include_str!("../input/day25.txt")),
+      1 => print_result(day, day01::compute, include_str!("../input/day01.txt")),
+      2 => print_result(day, day02::compute, include_str!("../input/day02.txt")),
+      3 => print_result(day, day03::compute, include_str!("../input/day03.txt")),
+      4 => print_result(day, day04::compute, include_str!("../input/day04.txt")),
+      5 => print_result(day, day05::compute, include_str!("../input/day05.txt")),
+      6 => print_result(day, day06::compute, include_str!("../input/day06.txt")),
+      7 => print_result(day, day07::compute, include_str!("../input/day07.txt")),
+      8 => print_result(day, day08::compute, include_str!("../input/day08.txt")),
+      9 => print_result(day, day09::compute, include_str!("../input/day09.txt")),
+      10 => print_result(day, day10::compute, include_str!("../input/day10.txt")),
+      11 => print_result(day, day11::compute, include_str!("../input/day11.txt")),
+      12 => print_result(day, day12::compute, include_str!("../input/day12.txt")),
+      13 => print_result(day, day13::compute, include_str!("../input/day13.txt")),
+      14 => print_result(day, day14::compute, include_str!("../input/day14.txt")),
+      15 => print_result(day, day15::compute, include_str!("../input/day15.txt")),
+      16 => print_result(day, day16::compute, include_str!("../input/day16.txt")),
+      17 => print_result(day, day17::compute, include_str!("../input/day17.txt")),
+      18 => print_result(day, day18::compute, include_str!("../input/day18.txt")),
+      19 => print_result(day, day19::compute, include_str!("../input/day19.txt")),
+      20 => print_result(day, day20::compute, include_str!("../input/day20.txt")),
+      21 => print_result(day, day21::compute, include_str!("../input/day21.txt")),
+      22 => print_result(day, day22::compute, include_str!("../input/day22.txt")),
+      23 => print_result(day, day23::compute, include_str!("../input/day23.txt")),
+      24 => print_result(day, day24::compute, include_str!("../input/day24.txt")),
+      25 => print_result(day, day25::compute, include_str!("../input/day25.txt")),
       _ => return Err("Invalid day".into()),
     }
   }
@@ -77,10 +77,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   Ok(())
 }
 
-fn print_result<D1: Display, D2: Display, F: Fn(&str) -> (D1, D2)>(f: F, s: &str) {
+fn print_result<D1: Display, D2: Display, F: Fn(&str) -> (D1, D2)>(day: usize, f: F, s: &str) {
   let start = Instant::now();
   let r = f(s);
   let end = Instant::now();
-  println!("{}, {}", r.0, r.1);
+  println!("Day {day} Part 1:\n{}\nDay {day} Part 2:\n{}", r.0, r.1);
   println!("took: {:?}", end - start);
 }
